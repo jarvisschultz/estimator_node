ee4d94e3676a41b29c6604295d0e46e2e6fec493
Jarvis Schultz <jschultz@u.northwestern.edu>
2011-09-16 02:10:02 -0700

    Just had the first working version that combines the kinect and the odometry.

--- src/robot_estimator_kinect_3d.cpp
+++ src/robot_estimator_kinect_3d.cpp
@@ -33,7 +33,7 @@
 // Global Variables
 //---------------------------------------------------------------------------
 #define NUM_CALIBRATES (30)
-#define CORRECTION_RATE (10)
+#define STDEV_RATE (10)
 
 //---------------------------------------------------------------------------
 // Objects and Functions
@@ -50,6 +50,8 @@ private:
     ros::Subscriber robot_kinect_sub;
     Eigen::Vector3d robot_start_pos;
     Eigen::Vector3d robot_cal_pos;
+    Eigen::Vector3d robot_estimate;
+    Eigen::Vector3d kinect_estimate;
     
     puppeteer_msgs::RobotPose pose;
     puppeteer_msgs::position_request position_request_srv;
@@ -65,6 +67,8 @@ private:
     unsigned int calibrate_count;
     tf::TransformListener tf;
     tf::TransformBroadcaster br;
+    float Kx, Ky, Kth;
+    unsigned int call_count;
 
             
 public:
@@ -80,7 +84,12 @@ public:
 	error_c = true;
 	calibrated_flag = false;  // We need to calibrate the sytem
 	calibrate_count = 0;
+	call_count = 0;
 
+	// Set gains:
+	Kx = 0.75; Ky = Kx;
+	Kth = 0.0;
+	
 	// initialize all state variables to zero
 	xc = 0.0; zc = 0.0; xc_last = 0.0; zc_last = 0.0;
 	th = 0.0; th_last = 0.0;
@@ -93,6 +102,7 @@ public:
     
     void trackercb(const puppeteer_msgs::PointPlus &point)
 	{
+	    ROS_DEBUG("trackercb triggered");
 	    static bool first_flag = true;
 	    error_kinect = point.error;
 	    // If need to calibrate, let's clear all of the values for the
@@ -144,6 +154,8 @@ public:
 		    robot_cal_pos = (robot_cal_pos/((float) NUM_CALIBRATES))-robot_start_pos;
 		    calibrate_count = 0;
 		    calibrated_flag = true;
+		    call_count = 0;
+		    Kth = 0.0;
 		    first_flag = true;
 		    return;
 		}		    
@@ -208,7 +220,6 @@ public:
 	    // check if the operating_condition parameter exists and set its value
 	    int operating_condition = 0;
 	    static bool first_flag = true;
-	    static unsigned int call_count = 0;
 	    
 	    if(ros::param::has("operating_condition"))
 	    {
@@ -231,38 +242,11 @@ public:
 	    // check to see if we are in run state
 	    if(operating_condition == 1 || operating_condition == 2)
 	    {
+		get_kinect_estimate();
 		if(calibrated_flag == true)
 		{
-		    make_robot_correction();
-		    // call_count++;
-		    // if (call_count%CORRECTION_RATE == 0)
-		    // {
-		    // 	if (error_kinect)
-		    // 	{
-		    // 	    call_count--;
-		    // 	    get_robot_estimate();
-		    // 	}
-		    // 	else
-		    // 	{
-		    // 	    ROS_INFO("Correcting robot's pose");
-		    // 	    make_robot_correction();
-		    // 	}
-		    // }
-		    // else
-		    // {
-		    // 	// Let's then get the robot's estimate of its state:
-		    // 	get_robot_estimate();
-		    // }
-		    // // fill out pose message:
-		    // pose.x_robot = xc;
-		    // pose.y_robot = zc;
-		    // pose.theta = th;
-		    // pose.error = error_c;
-		    // // set time stamp
-		    // pose.header.stamp = t_now_timer;
-		    // pose.header.frame_id = "/optimization_frame";
-		    // // publish system state
-		    // pose_pub.publish(pose);
+		    get_robot_estimate();
+		    get_optimal_estimate();
 		}
 		return;
 	    }
@@ -280,74 +264,6 @@ public:
 
 	    calibrated_flag = false;
 	    calibrate_count = 0;
-	}
-
-    void make_robot_correction(void)
-	{
-	    // check to see if robot index parameter exists.
-	    static int robot_index = 0;
-	    if(ros::param::has("robot_index"))
-	    {
-		ros::param::get("/robot_index", robot_index);
-	    }
-	    else
-	    {
-		ROS_WARN("Cannot Find Parameter: robot_index");
-		ROS_INFO("Setting robot_index to 1");
-		ros::param::set("/robot_index", 1);
-	    }
-
-	    // So, let's call the service to send the update command
-	    // to the robot:
-	    float x, y, theta;
-	    x = transformed_robot.point.x;
-	    y = transformed_robot.point.z;
-
-	    // estimate theta:
-	    theta = atan2(y-transformed_robot_last.point.z,
-		       x-transformed_robot_last.point.x);
-	    while(theta <= 0)
-		theta += 2.0*M_PI;
-	    while(th > 2.0*M_PI)
-		theta -= 2.0*M_PI;
-
-	    // correct robot's estimate:
-	    srv.request.robot_index = robot_index;
-	    srv.request.type = 'l';
-	    srv.request.Vleft = x;
-	    srv.request.Vright = y;
-	    srv.request.Vtop = theta;
-	    srv.request.div = 4;
-
-	    // // send request to service
-	    // if(correction_client.call(srv))
-	    // {
-	    // 	if(srv.response.error == false)
-	    // 	    ROS_DEBUG("Send Successful: speed_command\n");
-	    // 	else
-	    // 	{
-	    // 	    ROS_DEBUG("Send Request Denied: speed_command\n");
-	    // 	    static bool request_denied_notify = true;
-	    // 	    if(request_denied_notify)
-	    // 	    {
-	    // 		ROS_INFO("Send Requests Denied: speed_command\n");
-	    // 		request_denied_notify = false;
-	    // 	    }
-	    // 	}
-	    // }
-	    // else 
-	    // 	ROS_ERROR("Failed to call service: speed_command\n");
-	    
-	    // fill out pose message:
-	    pose.x_robot = x;
-	    pose.y_robot = y;
-	    pose.theta = theta;
-	    pose.error = error_c;
-	    // set time stamp
-	    pose.header.stamp = ros::Time::now();
-	    pose.header.frame_id = "/optimization_frame";
-	    // publish system state
-	    pose_pub.publish(pose);
 	}
 
     void get_robot_estimate(void)
@@ -420,7 +336,107 @@ public:
 		zc += zc_dot*dt_timer;
 		th += th_dot*dt_timer;
 	    }
-	}	    
+	    robot_estimate << xc, zc, th;
+	}
+
+    void get_kinect_estimate(void)
+	{
+	    static float th_array[STDEV_RATE];
+	    static float tmp[STDEV_RATE] ;
+	    static float stdev;
+	    static float theta_last = 0.0;
+	    static float alpha = 0.1;
+	    float theta = 0.0, x = 0.0, z = 0.0;
+	    unsigned int i = 0;
+	    x = transformed_robot.point.x;
+	    z = transformed_robot.point.z;
+	    
+	    kinect_estimate << x, z, 0.0;
+		
+	    theta = atan2(z-transformed_robot_last.point.z,
+			  x-transformed_robot_last.point.x);
+	    
+	    // Let's low pass filter this:
+	    theta = alpha*theta+(1-alpha)*theta_last;
+	    theta_last = theta;
+	    
+	    while(theta <= 0)
+		theta += 2.0*M_PI;
+	    while(th > 2.0*M_PI)
+		theta -= 2.0*M_PI;
+
+	    if (call_count < STDEV_RATE)
+	    {
+		th_array[call_count] = theta;
+		call_count++;
+		kinect_estimate(2) = theta;
+		return;
+	    }
+	    kinect_estimate(2) = theta;
+
+
+	    // Copy old array, and then set new array:
+	    memcpy(tmp, th_array, sizeof(th_array));
+	    th_array[0] = theta;	    
+	    for(i=1; i<STDEV_RATE; i++)
+		th_array[i] = tmp[i-1];
+
+	    stdev = calculate_stdev(th_array, STDEV_RATE);
+
+	    Kth = 0.01*sqrtf(1+powf(stdev,2.0))/stdev;
+	    if(Kth >= 1.0) Kth = 1.0;
+	    ROS_DEBUG("Changing the gain on theta to %f", Kth);
+	    return;
+	}
+
+    float calculate_stdev(const float ar[], const unsigned int size)
+	{
+	    // First calculate the mean:
+	    unsigned int i = 0;
+	    float mean = 0.0;
+	    float stdev = 0.0;
+	    for (i=0; i<size; i++)
+		mean += ar[i];
+	    mean /= ((float) size);
+
+	    // Now, the standard deviation:
+	    for (i=0; i<size; i++)
+		stdev += powf((ar[i]-mean),2.0);
+	    stdev /= ((float) size);
+	    stdev = sqrtf(stdev);
+	    return stdev;
+	}
+
+    void get_optimal_estimate(void)
+	{
+	    Eigen::Vector3d optimal_estimate, gains;
+	    gains << Kx, Ky, Kth;
+	    // gains << 0.0, 0.0, 0.0;
+	    ROS_INFO("Kth = %f",Kth);
+	    printf("Kinect = %f\t Robot = %f\n",kinect_estimate(2),
+		   robot_estimate(2));
+	    
+	    // optimal_estimate = robot_estimate.array() + gains.array()*
+	    // 	(kinect_estimate.array()-robot_estimate.array());
+	    for(unsigned int i = 0; i < 3; i++)
+		optimal_estimate(i) = robot_estimate(i)+
+		    gains(i)*(kinect_estimate(i)-robot_estimate(i));
+	    printf("Optimal = %f\n",optimal_estimate(2));
+	    
+	    pose.x_robot = optimal_estimate(0);
+	    pose.y_robot = optimal_estimate(1);
+	    pose.theta = optimal_estimate(2);
+	    // pose.x_robot = robot_estimate(0);
+	    // pose.y_robot = robot_estimate(1);
+	    // pose.theta = robot_estimate(2);
+	    pose.error = error_c;
+	    pose.header.stamp = ros::Time::now();
+	    pose.header.frame_id = "/optimization_frame";
+
+	    pose_pub.publish(pose);
+
+	    return;
+	}
 };
 
 
@@ -430,12 +446,16 @@ public:
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "robot_estimator_3d");
-  ros::NodeHandle node;
+    ROSCONSOLE_AUTOINIT;
+    
+    ros::init(argc, argv, "robot_estimator_3d");
+    // log4cxx::LoggerPtr my_logger = log4cxx::Logger::getLogger(ROSCONSOLE_DEFAULT_NAME);
+    // my_logger->setLevel(ros::console::g_level_lookup[ros::console::levels::Debug]);
+    ros::NodeHandle node;
 
-  PoseEstimator pose_estimator;
+    PoseEstimator pose_estimator;
 
-  ros::spin();
+    ros::spin();
 
-  return 0;
+    return 0;
 }
